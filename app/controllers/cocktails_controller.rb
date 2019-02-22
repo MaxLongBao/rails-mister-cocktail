4e247a5d4957299c6610c17da42c62b4d2bf1e93
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-02-22 16:43:39 +0000

    controller index done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,4 +2,8 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def show
+    @cocktails = Cocktail.find(params[:name])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Creating ingredients...'
-20.times do
-  ingredient = Ingredient.new(
-    name: Faker::Food.ingredient,
-  )
-  ingredient.save!
-end
-puts 'Finished!'
 
-puts 'Creating ingredients...'
-20.times do
-  cocktail = Cocktail.new(
-    name: Faker::Food.ingredient,
-  )
-  cocktail.save!
-end
-puts 'Finished!'
+# puts 'Creating ingredients...'
+# 20.times do
+#   ingredient = Ingredient.new(
+#     name: Faker::Food.ingredient
+#   )
+#   ingredient.save!
+# end
+# puts 'Finished!'
+
+# puts 'Creating ingredients...'
+# 20.times do
+#   cocktail = Cocktail.new(
+#     name: Faker::Food.name
+#   )
+#   cocktail.save!
+# end
+# puts 'Finished!'
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'gin')
+Ingredient.create(name: 'tonic water')
+Cocktail.create(name: 'Gin and Tonic')
+Cocktail.create(name: 'Mojito')
